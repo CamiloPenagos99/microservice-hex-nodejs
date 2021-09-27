ae3af97b7aca07c500447b872910e75c0f076a5d
PaolaTeran <paoteranortega@gmail.com>
2021-09-27 09:24:28 -0500

    feat: se agrega mock

--- src/util/Envs.ts
+++ src/util/Envs.ts
@@ -1,4 +1,7 @@
 export const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'development';
 
 export const GCP_PROJECT = process.env.GCP_PROJECT || 'cm-api-tracking-dev';
+
+export const PREFIX = process.env.SERVICE_NAME || 'cm-tracking-almacenar-pin-guia';
+
 console.log('GCP PROJECT', GCP_PROJECT);
