c55f41efa6c29a10663793ea1fc41f9ed311c8cd
camilo penagos <penagoscamilo99@gmail.com>
2021-11-10 11:36:06 -0500

    feat: cambio en el getURIBase para swagger URL

--- src/util/URL.ts
+++ src/util/URL.ts
@@ -1,6 +1,6 @@
 export const getURIBase = (node_env: string): string => {
     if (node_env.includes('local')) return 'localhost:8080';
-    if (node_env.includes('-test')) return '';
+    if (node_env.includes('testing')) return '';
     if (node_env === 'development') return 'cm-tracking-almacenar-pin-guia-lodr37gsga-uc.a.run.app';
     return 'localhost:8080';
 };
