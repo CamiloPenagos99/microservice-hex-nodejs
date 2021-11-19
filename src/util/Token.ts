26e27be8654afd5642517610f34a235eaf037722
camilo penagos <penagoscamilo99@gmail.com>
2021-11-19 17:01:29 -0500

    feat: manejo de error en createJWToken

--- src/util/Token.ts
+++ src/util/Token.ts
@@ -2,6 +2,12 @@ import { firebaseAdmin } from '@infrastructure/repositories/firestore/adapter/Fi
 
 export const getToken = async (payload: string): Promise<string> => {
     // const uid = v1();
-    const token = await firebaseAdmin.auth().createCustomToken(payload);
-    return token;
+    try {
+        const token = await firebaseAdmin.auth().createCustomToken(payload);
+        return token;
+    } catch (e) {
+        console.error('error generar pin', e.message);
+    }
+
+    return '';
 };
